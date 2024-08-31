0aef045e1077a2749605be3847659c7e688a6583
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-08-31 14:40:28 +0700

    useState Hook

--- project_reactjs/tiktok/src/App.js
+++ project_reactjs/tiktok/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+//Làm ứng dụng đếm số, nhấn vào button nó sẽ tăng lên 1 2 3 4 5
+import { useState } from 'react'
+
 
 function App() {
+ 
+  const [info, setInfo] = useState({
+    name: "Nguyen Van A",
+    age: 20,
+    address: "Ha Noi"
+  })
+
+  const handleUpdate = () => {
+    setInfo({
+      ...info,
+      bio: 'I am a software engineer'
+    })
+  }
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{padding: 20}}>
+      //Comment: chuyển object thành chuỗi
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>Update</button>
+      
     </div>
   );
 }
