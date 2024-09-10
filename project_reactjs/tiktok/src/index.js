fbb07f002282cd99eaa24ce8d86aa50c3b34326f
Nguyen QuynhGiang <nguyenquynhgiang1324@gmail.com>
2024-09-10 15:00:33 +0700

    useEffect with fake Chat App

--- project_reactjs/tiktok/src/index.js
+++ project_reactjs/tiktok/src/index.js
@@ -4,6 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Fake comment
+//Custom event để phát event tùy ý
+function emitComment(id){
+  setInterval(() => {
+    window.dispatchEvent(
+      new CustomEvent(`lesson-${id}`,{
+        detail: `Nội dung comment của lesson ${id}`
+      }
+      )
+    )
+  }, 2000);
+}
+
+emitComment(1);
+emitComment(2);
+emitComment(3);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
